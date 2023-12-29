e7a1ab4e32759c1c2694533bd4e2b47a2f17a0e4
Zuza Tatar <149330640+malzalanz@users.noreply.github.com>
2023-12-29 15:48:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -160,7 +160,7 @@ class Enemy(Ship):
 
 def collide(obj1, obj2):
     offset_x = obj2.x - obj1.x
-    offset_y = obj2.y - obj1.y  # Poprawiony błąd: Poprawiono obliczenia offsetu Y.
+    offset_y = obj2.y - obj1.y 
     return obj1.mask.overlap(obj2.mask, (offset_x, offset_y)) is not None
 
 def main():
